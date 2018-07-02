fa8275316bc074263ae3bb70166364c28a9c5622
Tomasz Banach <tomasz.banach@britenet.com.pl>
2018-07-02 15:10:36 +0200

    $http from api works ok

--- src/js/controllers/usersController.js
+++ src/js/controllers/usersController.js
@@ -1,8 +1,22 @@
 var usersApp = angular.module('usersApp')
 
 
-usersApp.controller('usersController', ['$scope', function ($scope) {
+usersApp.controller('usersController', ['$scope', '$http', function ($scope, $http) {
     $scope.message = 'Hello Users!'
+
+    $scope.users = []
+
+    $http({
+        method: 'GET',
+        url: 'https://randomuser.me/api/?results=5'
+     }).then(function (response){
+        console.log(response.data.results)
+        $scope.users = response.data.results
+     },function (error){
+        console.log('data ' + data)
+        console.log('status ' + status)
+     });
+
 }
 ])
 
