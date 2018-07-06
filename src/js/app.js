4a025e6651fadb5c90bcdea59014302dc3807e0a
Tomasz Banach <tomasz.banach@britenet.com.pl>
2018-07-06 10:34:59 +0200

    app works correctly

--- src/js/app.js
+++ src/js/app.js
@@ -9,24 +9,24 @@ usersApp.config(['$stateProvider', ($stateProvider) => {
         templateUrl: 'views/users.html',
         controller: 'usersController'
     })
-    .state({
-        name: 'user-details',
-        url: '/user-details/:id',
-        templateUrl: 'views/user-details.html',
-        controller: 'userDetailsController'
-    });
+        .state({
+            name: 'user-details',
+            url: '/user-details/:id',
+            templateUrl: 'views/user-details.html',
+            controller: 'userDetailsController'
+        });
 }
 ]);
+
 
 usersApp.service('passingUserService', function ($http) { // eslint-disable-line
 
     this.userArray = $http({
         method: 'GET',
-        url: 'https://randomuser.me/api/?results=10&nat=US&seed=abc'
+        url: 'https://randomuser.me/api/?results=10&nat=US&seed=a'
     }).then((response) => {
 
         const gridData = response.data.results;
-
         const gridInput = gridData.map((el) => {
             const rowObject = {};
             rowObject.thumbnail = el.picture.thumbnail;
@@ -35,35 +35,9 @@ usersApp.service('passingUserService', function ($http) { // eslint-disable-line
             rowObject.lastName = el.name.last.charAt(0).toUpperCase() + el.name.last.slice(1);
             rowObject.age = el.dob.age;
             rowObject.gender = el.gender;
-            rowObject.id = el.id.value;
+            rowObject.id = el.login.uuid;
             return rowObject;
         });
         return gridInput;
-    },
-    (error) => {});
+    },(error) => { });
 });
-
-usersApp.service('idService', function () { // eslint-disable-line
-        
-      this.gridUpdate = 
-    {
-        enableSorting: true,
-        rowHeight: 50,
-        columnDefs: [
-            { field: 'thumbnail', enableSorting: false, cellClass: 'thumbnailCell', cellTemplate: '<img ng-src=\'{{grid.getCellValue(row, col)}}\'>' },
-            { field: 'firstName', enableSorting: false, cellClass: 'thumbnailCell' },
-            { field: 'lastName',
-                enableSorting: false,
-                cellClass: 'thumbnailCell',
-                cellTemplate: '<a ng-click="grid.appScope.cellClicked(row,col)" ui-sref="user-details({id: row.entity.id })" class="ui-grid-cell-contents" title="TOOLTIP">{{COL_FIELD CUSTOM_FILTERS}}</a>' },
-            { field: 'age', enableSorting: false, cellClass: 'thumbnailCell' },
-            { field: 'gender', cellClass: 'thumbnailCell' }
-        ]
-        
-    };
-    
-  
-   
-});
-
-// apiKey: ('AIzaSyByYJ0xFVKhi5Ro8jp1600xtlI5bFr8VnE')
--- src/js/controllers/usersController.js
+++ src/js/controllers/usersController.js
@@ -1,19 +1,26 @@
-usersApp.controller('usersController', ['$scope', '$http', '$stateParams', 'passingUserService', 'idService', function ($scope, $http, $stateParams, passingUserService, idService) { // eslint-disable-line
+usersApp.controller('usersController', ['$scope', '$http', '$stateParams', 'passingUserService', function ($scope, $http, $stateParams, passingUserService) { // eslint-disable-line
 
-    
-    
-    
-    $scope.cellClicked = (row, col) => {
-        console.log('userDataObject', row.entity.id);
-    };
-    
+    $scope.gridOptions = { rowHeight: 50 };
+
     $scope.passingUserService = passingUserService.userArray;
 
-    $scope.passingUserService.then((data) => {
+    passingUserService.userArray.then((data) => {
         $scope.gridOptions = {
+            enableSorting: true,
+            rowHeight: 50,
+            columnDefs: [
+                { field: 'thumbnail', enableSorting: false, cellClass: 'thumbnailCell', cellTemplate: '<img ng-src=\'{{grid.getCellValue(row, col)}}\'>' },
+                { field: 'firstName', enableSorting: false, cellClass: 'thumbnailCell' },
+                {
+                    field: 'lastName',
+                    enableSorting: false,
+                    cellClass: 'thumbnailCell',
+                    cellTemplate: '<a ng-click="grid.appScope.cellClicked(row,col)" ui-sref="user-details({id: row.entity.id })" class="ui-grid-cell-contents" title="TOOLTIP">{{COL_FIELD CUSTOM_FILTERS}}</a>'
+                },
+                { field: 'age', enableSorting: false, cellClass: 'thumbnailCell' },
+                { field: 'gender', cellClass: 'thumbnailCell' }
+            ],
             data
         };
     });
-
-    $scope.gridOptions = idService.gridUpdate
 }]);
